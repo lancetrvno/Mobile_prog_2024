839b5838b6b6464c3a7ff680578711558ffe441e
Lance <trivinolance2004@gmail.com>
2024-09-07 10:18:20 +0800

    first commit

---
+++ play_detruc.js
@@ -0,0 +1,13 @@
+// ? Destructuring objects and arrays
+// Sept 2024
+
+const person = {
+    name: 'John',
+    age: 25,
+    hobbies: ['reading', 'cooking'],
+    greet: function(){
+    console.log ('Hello, my name is ' + this. name + ' and I am' + this.age + ' years old.');
+    }
+    };
+    // When to use destructuring?
+    // Destructuring is useful when you only need a specific property of an object.
